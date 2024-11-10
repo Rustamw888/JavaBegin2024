bc941b941c50e573e1fc289142427e6330b2f258
qaMilord <rustamw888@gmail.com>
2024-11-11 01:35:37 +0300

    common commit

--- src/ru/javabegin/oop/printer/MainPrinter.java
+++ src/ru/javabegin/oop/printer/MainPrinter.java
@@ -1,5 +1,7 @@
 package ru.javabegin.oop.printer;
 
+import ru.javabegin.oop.computer.Main;
+
 public class MainPrinter {
 
   public static void main(String[] args) {
@@ -9,6 +11,9 @@ public class MainPrinter {
     printer.charge(5);
     printer.print("Hello world");
     printer.print("test");
+    Printer pr = (Printer)printer;
+    pr.setPaintVolume(4321423);
+    IPrinter pr2 = (IPrinter) new MainPrinter();
   }
 
 }
